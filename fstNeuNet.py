2c87f68ebc8b39f0c90aca5a4fc7bc1823ed60b2
Pawan Nirpal <49726539+pawan-nirpal-031@users.noreply.github.com>
2019-09-11 12:57:26 +0530

    Update fstNeuNet.py

--- fstNeuNet.py
+++ fstNeuNet.py
@@ -47,24 +47,56 @@ w2y = np.random.randn()
 
 bias = np.random.randn()
 
-# x1 = int(input("Enter patel length"))
-# y1 = float(input("Enter patel width"))
+
 # out = node(x1, y1, w1x, w2y, bias)
 # print('chances of this flower being red close to (1) are {}'.format(out))
-p =-10
 
 
-for i in range(50):
-    p = p - 0.1 * slope(p)
-    print(p)
-
-
-X = np.linspace(-20, 20, 100)
+X = np.linspace(-6, 6, 100)
 
 Y = deriv_sigmoid(X)
+# plt.title('Sigmoid function in Neural Network Training')
+# plt.plot(X, sigmoid(X), label='Sigmoid(x)', c='r')
+# plt.plot(X, deriv_sigmoid(X), label='Derivative_of_Sigmoid(x)', c='b')
+# plt.legend()
+# plt.xlabel("X axis ")
+# plt.ylabel("Y axis ")
+# plt.show()
 
-plt.plot(X, Y)
 
-plt.xlabel = 'X axis'
-plt.ylabel = 'Y axis'
-plt.show()
+
+for i in range(len(data)): # scatter plot of data
+    plt.axis([0, 6, 0, 6])
+    point = data[i] # point is actually this [length, width, type], where type is 0 or 1
+    color = 'r'
+    if point[2] == 0:
+        color = 'b'
+    plt.scatter(point[0], point[1], c=color)
+
+
+#plt.grid()
+#plt.show()
+learning_rate = 0.1
+
+
+for i in range(10000):  # training loop
+    ri = np.random.randint(int(len(data)))
+    point = data[ri]
+    z = point[0]*w1x + point[1]*w2y + bias
+    pred = sigmoid(z)
+    target = point[2]
+    cost = np.square(pred - target)
+    if i%1000 == 0:
+        print('Cost now : ', cost)
+    deriv_cost = 2*(pred - target)
+    deriv_predWz = deriv_sigmoid(z)
+    dwz_w1 = point[0]
+    dwz_w2 = point[1]
+    dz_b = 1
+    dcost_dz = deriv_cost*deriv_predWz
+    dcost_dw1 = dcost_dz*dwz_w1
+    dcost_dw2 = dcost_dz*dwz_w2
+    dcost_db = dcost_dz*dz_b
+    w1x = w1x - learning_rate*dcost_dw1
+    w2y = w2y - learning_rate*dcost_dw2
+    bias = bias - learning_rate*dcost_db
